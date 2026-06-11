85bd091a5d847644e99cab55e60c60ad22c881ea
Hiep50 <nhhiep0416@gmail.com>
2026-06-11 11:22:33 +0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/Pha-kh-ng-sinh/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
